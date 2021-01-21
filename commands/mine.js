6a3849c2ae22ff73621136652a5f64d9e7f6bf01
ruga123 <67188261+ruga123@users.noreply.github.com>
2021-01-21 09:36:55 +0700

    Update mine.js

--- commands/mine.js
+++ commands/mine.js
@@ -56,16 +56,17 @@ module.exports.run = async (bot, message, args) => {
             newData.save().catch(err => console.log(err))
            return message.reply(`Your mining session is now in progress for ${Math.round(time/60000)} minutes`);
         } else {
-            if(data.minetime - (Date.now() -data.mining) > 0){
-            return message.reply(`Your mining session is still in progress for ${Math.round((data.minetime - (Date.now() -data.mining))/60000)} minutes`);
-            } else {
-                if(data.mine==0&&data.minetime==0)
+            if(data.mine==-1&&data.minetime==-1&&!args[0])
                 {
                     data.minetime=time;
                     data.mining = Date.now();
                     data.save().catch(err => console.log(err))
                     return message.reply(`Your mining session is now in progress for ${Math.round(time/60000)} minutes`);
                 }
+            if(data.minetime - (Date.now() -data.mining) > 0){
+            return message.reply(`Your mining session is still in progress for ${Math.round((data.minetime - (Date.now() -data.mining))/60000)} minutes`);
+            } else {
+                
                 var eff=data.eff;
                 data.stone+=Math.abs(Math.floor((functions.range(30,50)*3.8*data.minetime/functions.range(1,3)/60000)*(eff+1)/3))
                 data.gravel+=Math.abs(Math.floor((functions.range(7,15)*0.8*data.minetime/functions.range(1,2)/60000)*(eff+1)/3))
@@ -76,8 +77,14 @@ module.exports.run = async (bot, message, args) => {
                 data.redstone+=Math.abs(Math.floor((functions.range(6,12)*0.5*data.minetime*(data.fortune+1)*functions.range(4,5)/60000)*(eff+1)/3))
                 data.lapis+=Math.abs(Math.floor((functions.range(3,8)*0.3*data.minetime*(data.fortune+1)*functions.range(4,8)/60000)*(eff+1)/3))
                 data.diam+=Math.abs(Math.floor(Math.floor((functions.range(0,20+Math.floor(data.minetime/2400000))*0.1)*data.minetime*(data.fortune+1)/60000)*(eff+1)/3))
-                data.minetime= 0;
-                data.mining = 0;
+                data.minetime= -1;
+                data.mining = -1;
+                if(args[0])
+                {
+                    data.minetime=time;
+                    data.mining = Date.now();
+                    return message.reply(`You have claimed your mining results and started your mining session for ${time} minutes!`);
+                }
                 data.save().catch(err => console.log(err))
                 return message.reply(`You have claimed your mining results!`);
             }
